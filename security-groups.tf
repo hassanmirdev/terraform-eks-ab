4ea1cd38e3c92880cfea3dd1d97409a9d3d51d19
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-31 17:20:32 +0530

    Update security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -1,15 +1,15 @@
 
-resource "aws_security_group" "all_worker_mgmt" {
+resource "aws_security_group" "my-sg" {
   name_prefix = "all_worker_management"
   vpc_id      = module.vpc.vpc_id
 }
 
-resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
+resource "aws_security_group_rule" "my-sg-ingress" {
   description       = "allow inbound traffic from eks"
   from_port         = 0
   protocol          = "-1"
   to_port           = 0
-  security_group_id = aws_security_group.all_worker_mgmt.id
+  security_group_id = aws_security_group.my-sg.id
   type              = "ingress"
   cidr_blocks = [
     "10.0.0.0/8",
@@ -18,11 +18,11 @@ resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
   ]
 }
 
-resource "aws_security_group_rule" "all_worker_mgmt_egress" {
+resource "aws_security_group_rule" "my-sg-egress" {
   description       = "allow outbound traffic to anywhere"
   from_port         = 0
   protocol          = "-1"
-  security_group_id = aws_security_group.all_worker_mgmt.id
+  security_group_id = aws_security_group.my-sg.id
   to_port           = 0
   type              = "egress"
   cidr_blocks       = ["0.0.0.0/0"]
